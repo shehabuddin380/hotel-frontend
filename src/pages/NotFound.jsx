79c74ef0523713f60c0a3e0a4e83f45c85874ebe
Shehab Uddin <shehabuddin380@gmail.com>
2026-01-19 23:07:38 +0600

    update ui

--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -3,9 +3,17 @@ import { Link } from "react-router-dom";
 const NotFound = () => {
   return (
     <div className="min-h-screen flex flex-col justify-center items-center bg-gray-100">
+      
       <h1 className="text-6xl font-bold text-red-500">404</h1>
-      <p className="mb-4">Page Not Found</p>
-      <Link to="/" className="bg-black text-white px-4 py-2 rounded">
+      
+      <p className="mt-2 mb-5 text-lg font-medium text-gray-700">
+        Page Not Found
+      </p>
+
+      <Link
+        to="/"
+        className="bg-black text-white px-5 py-2 rounded hover:bg-gray-800"
+      >
         Go Home
       </Link>
     </div>
