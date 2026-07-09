8a8d0383daae3e6422186721d1285a2345ec33b7
Shihab Ahmed <shehabuddin380@gmail.com>
2026-07-09 21:20:12 +0600

    upadet ProtectedRoute

--- src/auth/ProtectedRoute.jsx
+++ src/auth/ProtectedRoute.jsx
@@ -1,10 +1,13 @@
-import { Navigate } from "react-router-dom";
+import { Navigate, useLocation } from "react-router-dom";
 
 const ProtectedRoute = ({ children }) => {
-  const token = localStorage.getItem("token") || sessionStorage.getItem("token");
+  const location = useLocation();
+
+  const token =
+    localStorage.getItem("token") || sessionStorage.getItem("token");
 
   if (!token) {
-    return <Navigate to="/" replace />;
+    return <Navigate to="/login" replace state={{ from: location }} />;
   }
 
   return children;
