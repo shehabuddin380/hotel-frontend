35e685b3ef452f8a55b6a56690a79a35500acb13
Shihab Ahmed <shehabuddin380@gmail.com>
2026-07-09 21:26:41 +0600

    upadet and fix NotFound

--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -2,17 +2,17 @@ import { Link } from "react-router-dom";
 
 const NotFound = () => {
   return (
-    <div className="min-h-screen flex flex-col justify-center items-center bg-gray-100">
-      
-      <h1 className="text-6xl font-bold text-red-500">404</h1>
-      
-      <p className="mt-2 mb-5 text-lg font-medium text-gray-700">
+    <div className="min-h-screen flex flex-col justify-center items-center bg-slate-950 text-white">
+
+      <h1 className="text-7xl font-bold text-yellow-400">404</h1>
+
+      <p className="mt-2 mb-5 text-lg font-medium text-slate-300">
         Page Not Found
       </p>
 
       <Link
         to="/"
-        className="bg-black text-white px-5 py-2 rounded hover:bg-gray-800"
+        className="bg-yellow-400 hover:bg-yellow-300 text-slate-900 font-semibold px-5 py-2 rounded-lg transition"
       >
         Go Home
       </Link>
