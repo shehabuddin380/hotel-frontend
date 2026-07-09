7f80edad892c5e97eecdbb802a8a3678163b0a6d
Shihab Ahmed <shehabuddin380@gmail.com>
2026-07-09 21:33:39 +0600

    upadet and fix Layout

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -4,14 +4,8 @@ import Navbar from "./Navbar";
 export default function Layout() {
   return (
     <>
-      {/* Navbar */}
       <Navbar />
-      {/* Page Content */}
-      <div className="min-h-screen bg-gray-100">
-        <div className="max-w-7xl mx-auto px-4 py-6">
-          <Outlet />
-        </div>
-      </div>
+      <Outlet />
     </>
   );
 }
